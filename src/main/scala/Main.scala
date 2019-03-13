87515d2ac4fcdd2ff1212be3b91211e9e66b2313
Diogo Santos <diogo@diogosantos.com>
2019-03-13 18:02:58 +0100

    Fixing the def instead of val

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,7 @@
 object Main extends App {
   require(args.length > 0, "Parameter `username` is required.")
+  val username = args(0)
 
-  def username = args(0)
-
+  val githubUserService = Components.githubUserService
   githubUserService.run(username)
-
-  def githubUserService = Components.githubUserService
 }
